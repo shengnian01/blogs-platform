7ea8411e41183f3e50e916c08174c55499f25798
master <648045014@qq.com>
2020-10-21 17:23:55 +0800

    图片上传七牛云

---
+++ blogs/src/main/java/com/example/blogs/util/VariableName.java
@@ -0,0 +1,12 @@
+package com.example.blogs.util;
+
+public class VariableName {
+    //    七牛AK
+    public static final String accessKey = "z7g8tHyLNQfj44htfsdGMdxLf5PIwy4yYslnpmfG";
+    //    七牛SK
+    public static final String secretKey = "p055Qss2kFEVEdDdLSnX-JqZvIgctmBc11I6Qt-K";
+    //    七牛存储空间名
+    public static final String bucket = "blog-nian";
+    //    七牛默认域名
+    public static final String domain = "zhangshengnian-xyz-idvdtqq.qiniudns.com";
+}
