a5700bcf06c2ef3c4c1bed63b908802f7eab2974
master <648045014@qq.com>
2020-10-24 17:30:07 +0800

    添加图片上传到服务器以来，以及后端跨域解决

--- blogs/src/main/java/com/example/blogs/controller/UserController.java
+++ blogs/src/main/java/com/example/blogs/controller/UserController.java
@@ -2,8 +2,11 @@ package com.example.blogs.controller;
 
 import com.example.blogs.pojo.User;
 import com.example.blogs.service.UserService;
+import com.example.blogs.util.QiniuUpload;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,5 +18,12 @@ public class UserController {
     public String getUser(){
         User user=userService.getUser(1l);
         return "my name is jack!"+user.toString();
+    }
+    @PostMapping("/fileUp")
+    public String fileUp(@RequestParam(value="path",required=true) String path,
+                         @RequestParam(value="name",required=true) String name){
+        System.out.println("fileup");
+        String str= QiniuUpload.UploadPic(path,name);
+        return str;
     }
 }
