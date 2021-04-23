851fe592745719cf02415fbf084c5370cbdd9fc5
master <648045014@qq.com>
2021-04-23 16:15:49 +0800

    接口调试

--- blogs/src/main/java/com/example/blogs/controller/UserController.java
+++ blogs/src/main/java/com/example/blogs/controller/UserController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/api/user")
 public class UserController {
     @Autowired
     private UserService userService;
